e8a14842911c14c907651ae55cb50a5208b9c04e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-07-20 21:46:48 +0100

    Helper function that filters for food events

--- helper/food.py
+++ helper/food.py
@@ -2,7 +2,11 @@ from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
 def find_food(data):
-    """Give me a file like object or a URL"""
+    """
+    Give me a file like object or a URL, returns True or False based on if it finds
+    food
+    """
+
     if type(data) == str or type(data) == unicode:
         data = urlopen(data)
     soup = BeautifulSoup(data)
@@ -12,3 +16,14 @@ def find_food(data):
             return True
 
     return False
+
+def give_food(events):
+    """Go through a list of events and return the ones that have food"""
+
+    output = []
+
+    for event in events:
+        if find_food(event.url):
+            output.append(event)
+
+    return output

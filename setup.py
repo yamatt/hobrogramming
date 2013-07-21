0ca5273c47dfacd7f2fa7adfcd3d1eaca6d6257f
Matt Copperwaite <mattcopp+git@gmail.com>
2013-07-21 05:42:29 +0100

    Added more features

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Hobrogrammer',
+      version='1.0',
+      description='Hobrogrammer guides you to your local events with free food.',
+      author='Matt Copperwaite',
+      author_email='none@none.com',
+      url='http://hobrogrammer.com',
+      install_requires=['Flask', 'SQLAlchemy', "BeautifulSoup"],
+)
--- wsgi/database.py
+++ wsgi/database.py
@@ -1,17 +1,24 @@
 from sqlalchemy import create_engine, Column, BigInteger, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class database(object):
-    def setup_database():
+    def setup_database(self):
         Base.metadata.create_all(self.engine)
 
-    def __init__(self, connection_str):
+    def __init__(self, connection_string):
         self.engine = create_engine(connection_string)
         Session = sessionmaker(self.engine)
         self.session = Session()
-
+        
+    def get_event(self, id):
+        return self.session.query(food_events).filter_by(id=id).first()
+        
+    def update_food(self, id, value):
+        food_event = self.get_event(id)
+        food_event.update_food(value)
 
 class food_events(Base):
     __tablename__ = "food_events"

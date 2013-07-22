b37f787fc7f939b52996997c863fb2665ea30824
Matt Copperwaite <mattcopp+git@gmail.com>
2013-07-22 13:43:54 +0100

    more error resistant and confirmation dialog

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -1,8 +1,16 @@
 
 $().ready(function () {
+    _.extend(oevents, Backbone.Events);
+    
+    // Setting up forms
     $('form').submit(function (e) {
         e.preventDefault();
-        events.fetch({'data': $(e.currentTarget).serialize()});
+        data = $(e.currentTarget).serialize()
+        if (data == "location=") {
+            data = "location=London%2C+UK"
+        }
+        events.fetch({'data': data});
+        $("#events").append("<li>Getting results...<li>")
     });
     
     $('#geo').click(function () {
@@ -11,16 +19,45 @@ $().ready(function () {
         });
     });
     
+    // Backbone Events
+    oevents.on("update", function (this_event) {
+        var popup = new PopupView({
+            model: this_event
+        });
+        
+        if (this_event.get("has_food")) {
+            popup.render(
+                "Change event?",
+                "Are you sure you wish to mark that " + this_event.get("title") + " <b> has no free food?</b>",
+                "Save change",
+                this_event.toggle_food
+            )
+        }
+        else {            
+            popup.render(
+                "Change event?",
+                "Are you sure you wish to mark that " + this_event.get("title") + " <b>has having free food?</b>",
+                "Save change",
+                this_event.toggle_food
+            )
+        }
+    });
+    
+    // Backbone MVC
+    
     var Event = Backbone.Model.extend({
         urlRoot: "/api/update",
-        toggle_food: function () {
-            this.save({food: !this.get('food')});
+        toggle_food: function (model) {
+            that = model || this
+            that.save({food: !that.get('food')});
         },
-        no_food: function () {
-            this.save({food: false});
+        no_food: function (model) {
+            that = model || this
+            that.save({food: false});
         },
-        has_food: function () {
-            this.save({food: true});
+        has_food: function (model) {
+            that = model || this
+            that.save({food: true});
         },
     });
     
@@ -29,8 +66,8 @@ $().ready(function () {
         className: "well",
         template: _.template($('#template_event').html()),
         events: {
+          "click .book" : "book",
           "click .food" : "toggle_food",
-          "click .book" : "book"
         },
         initialize: function() {
             this.listenTo(this.model, 'change', this.render);
@@ -47,7 +84,7 @@ $().ready(function () {
             window.location.href = this.model.get("url");
         },
         toggle_food: function () {
-            this.model.toggle_food();
+            oevents.trigger("update", this.model);
         }
     });
     
@@ -59,7 +96,7 @@ $().ready(function () {
                 return response.events;
             }
             else {
-                throw("Returned data failed: " + response);
+                throw("Returned data failed: " + response.message);
             }
         }
     });
@@ -85,6 +122,35 @@ $().ready(function () {
     });
     
     var events_view = new EventsView;
+    
+    var PopupView = Backbone.View.extend({
+        template: _.template($('#template_popup').html()),
+        events: {
+          "click .ok" : "ok",
+          "click .cancel" : "cancel"
+        },
+        render: function(title, content, button_label, success) {
+            this.success = success
+            this.$el.html(
+                this.template({'title': title, 'content': content, 'button_label': button_label})
+            );
+            $('body').append(this.$el);
+            this.popup = $(this.$el.children()[0]);
+            this.popup.modal('show');
+        },
+        hide: function() {
+            this.popup.modal('hide');
+        },
+        ok: function () {
+            this.success(this.model);
+            this.hide();
+        },
+        cancel: function () {
+            this.hide();
+        }
+    });
 });
+
+var oevents = {};
 
 
